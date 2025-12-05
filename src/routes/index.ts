d559e98ab7ddd04f9285e307af9f79b31d036654
brunomffl <brunocardoso07@Outlook.com>
2025-12-05 20:49:39 -0300

    feat: Cria middleware de autenticação

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { authRoutes } from "./authRoutes";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 
 const routes = Router();
 
+//rotas públicas
 routes.use("/auth", authRoutes);
+
+//rotas protegidas
+routes.use(ensureAuthenticated);
 
 export { routes };

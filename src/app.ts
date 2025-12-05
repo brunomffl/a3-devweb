1180b03821a3170f3142487c6f32ccdc12765c4f
brunomffl <brunocardoso07@Outlook.com>
2025-12-05 20:47:44 -0300

    fix: Ajusta falta de middleware de tratamento de erro no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { routes } from "./routes";
+import { errorHandling } from "./middlewares/errorHandling";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
-//app.use(errorHandling);
+app.use(errorHandling);
 
 export { app };

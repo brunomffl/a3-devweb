5cc80a981878f614e853591fb38ba1207e000833
brunomffl <brunocardoso07@Outlook.com>
2025-12-05 21:02:27 -0300

    fix: Altera a versão do prisma para ser compatível com a propriedade necessária

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import { AuthController } from "@/controllers/authController";
+import { validateBody } from "@/middlewares/validate-schema";
+import { authRegisterSchema, authSchema } from "@/schemas/authSchema";
 
 const authRoutes = Router();
 const authController = new AuthController();
 
-authRoutes.post("/login", 
+authRoutes.post("/login",
+    validateBody(authSchema),
     authController.login.bind(authController)
 );
 
 authRoutes.post("/register",
+    validateBody(authRegisterSchema),
     authController.register.bind(authController),
 );
 
